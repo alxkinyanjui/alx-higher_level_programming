939e25d96173d25cd09c8548f2c6ed9740d4e506
alxkinyanjui <alexgkinyanjui@gmail.com>
2023-03-20 07:59:14 -0700

    All Tasks

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""List all states that matches the arg"""
+
+from sys import argv
+import MySQLdb
+
+
+def my_filter_states():
+    """ SQL INFO FROM ARGV """
+    sql_usrname = argv[1]
+    sql_password = argv[2]
+    sql_database = argv[3]
+    usr_input = argv[4]
+
+    host = "localhost"
+    port = 3306
+
+    """ SETTING MySQLdb Connection """
+    db_connection = MySQLdb.connect(
+        port=port,
+        host=host,
+        user=sql_usrname,
+        password=sql_password,
+        database=sql_database
+        )
+
+    cur = db_connection.cursor()
+
+    """ EXECUTING SQL QUERY """
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+             ORDER BY states.id ASC".format(usr_input))
+
+    """ FETCHING DATA """
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    db_connection.close()
+
+
+if __name__ == '__main__':
+    my_filter_states()
